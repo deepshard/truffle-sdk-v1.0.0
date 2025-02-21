022ebb612bf611fd2bd699755320ce5e3fc00028
saint <saint.0x.saint@gmail.com>
2025-02-21 05:47:18 -0500

    cleanup

--- packages/__init__.py
+++ packages/__init__.py
@@ -5,4 +5,6 @@ This module provides the Truffle SDK for building AI-powered tools.
 from .cli.src.main import main
 from .sdk.src import *
 
-__version__ = "1.0.0" 
+__version__ = "1.0.0"
+
+"""Root package for Truffle tools.""" 
