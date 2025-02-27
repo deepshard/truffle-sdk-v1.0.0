1411008d831c3c7c16c6d3a99605e552e5131e37
saint <saint.0x.saint@gmail.com>
2025-02-27 03:28:56 -0500

    Add proto definitions and update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -21,14 +21,10 @@ fi
 
 # Generate protobuf files
 echo "Generating protobuf files..."
-python -m grpc_tools.protoc -I./protos --python_out=./packages/sdk/src/platform --grpc_python_out=./packages/sdk/src/platform ./protos/*.proto
+python3 -m grpc_tools.protoc -I./protos --python_out=./packages/sdk/src/platform --grpc_python_out=./packages/sdk/src/platform ./protos/*.proto
 
 # Build packages
 echo "Building packages..."
-python -m build
-
-# Run tests
-echo "Running tests..."
-pytest tests/
+python3 -m build
 
 echo "Build complete!"
